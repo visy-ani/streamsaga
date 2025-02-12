f218e7bdb8c4bdce43caff1a490d48511decd51d
chocoanish <chocoboyanish566@gmail.com>
2025-02-13 00:11:25 +0530

    Add NextAuth authentication with credentials provider and Prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
+const baseprisma = new PrismaClient();
 const prisma = new PrismaClient().$extends(withAccelerate())
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
+
+export { PrismaAdapter, baseprisma }
 export default prisma
